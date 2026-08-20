e037ac9eff667c4d6fae7a7e029bcfc4f8c5fc24
biagoncalvezx <beatriz.g.campos@aluno.senai.br>
2026-08-20 12:06:29 -0300

    20/08

---
+++ javascript/WebStorage/atividade1.js
@@ -0,0 +1,116 @@
+class Produto {
+    constructor(nome, preco, categoria, desconto) {
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+        this.desconto = desconto;
+    }
+
+    aplicarDesconto() {
+        const precoComDesconto = this.preco - (this.preco * (this.desconto / 100));
+        return precoComDesconto;
+    }
+
+    exibir() {
+        const resultado = document.querySelector('#resultado');
+        
+        resultado.innerHTML = `
+        <div> 
+            <p>Nome: ${this.nome}</p>   
+            <p>Preço: R$ ${this.aplicarDesconto().toFixed(2)}</p>
+            <p>Categoria: ${this.categoria}</p>
+            <p>Desconto: ${this.desconto}%</p>
+        </div>
+        `;
+    }   
+}
+
+
+class Produtos {
+    constructor() {
+        this.produtos = [];
+    }
+
+    adicionarProduto(produto) {
+        this.produtos.push(produto);
+
+        
+        this.salvarNoLocalStorage();
+    }
+
+    excluirProduto(indice) {
+        this.produtos.splice(indice, 1);
+
+        
+        this.salvarNoLocalStorage();
+
+        this.exibirNaTela();
+    }
+
+    salvarNoLocalStorage() {
+        localStorage.setItem('produtos', JSON.stringify(this.produtos));
+    }
+
+    carregarDoLocalStorage() {
+        const produtosSalvos = localStorage.getItem('produtos');
+
+        if (produtosSalvos) {
+            const produtos = JSON.parse(produtosSalvos);
+
+            this.produtos = produtos.map(produto => {
+                return new Produto(
+                    produto.nome,
+                    produto.preco,
+                    produto.categoria,
+                    produto.desconto
+                );
+            });
+        }
+
+        this.exibirNaTela();
+    }
+
+    exibirNaTela() {
+        const resultado = document.querySelector('#resultado');
+        resultado.innerHTML = "";
+
+        this.produtos.forEach((produto, indice) => {
+            resultado.innerHTML += `
+            <div> 
+                <p>Nome: ${produto.nome}</p>
+                <p>Preço: R$ ${produto.aplicarDesconto().toFixed(2)}</p>
+                <p>Categoria: ${produto.categoria}</p>
+                <p>Desconto: ${produto.desconto}%</p>
+
+                <button onclick="produtos.excluirProduto(${indice})">
+                    Excluir
+                </button>
+            </div>
+            `;
+        });
+    }
+}
+
+
+const nome = document.querySelector('#nome');
+const preco = document.querySelector('#preco');
+const categoria = document.querySelector('#categoria');
+const desconto = document.querySelector('#desconto');
+const botaoCadastrar = document.querySelector('#botaoCadastrar');
+
+const produtos = new Produtos();
+
+produtos.carregarDoLocalStorage();
+
+
+botaoCadastrar.addEventListener('click', function () {
+    const produto = new Produto(
+        nome.value, 
+        Number(preco.value), 
+        categoria.value, 
+        Number(desconto.value)
+    );
+
+    produtos.adicionarProduto(produto);
+    produtos.exibirNaTela();
+});
